898d057d1d6e997dfabfbd5438bacb4012f2b383
Nick K <nkokkos@gmail.com>
2018-01-22 10:04:25 +0000

    added foreman support with Procfile and .env file so it starts at 3000 port

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -6,5 +6,9 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
+// frontend/packs/application.js
 
+import "./application.css";
+
+document.body.insertAdjacentHTML("afterbegin", "Webpacker works");
 console.log('Hello World from Webpacker')
