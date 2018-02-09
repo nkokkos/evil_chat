83c63ca667709baf6f471776590c1d23125a2e21
Nick K <nkokkos@gmail.com>
2018-02-09 14:39:20 +0000

    entered chat show action

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,2 +1,16 @@
 class ChatController < ApplicationController
+  before action: autheticate!
+
+  # display last 20 messages
+  def show
+    @messages = Message.order(created_at: :asc).last(20)
+  end
+
+  private
+  
+  # redirect user to /login if he hasn't picked a username yet
+  def authenticate!
+    redirect_to login login_path unless session[:username]
+  end
+
 end
